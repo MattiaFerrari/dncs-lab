3486014b6ed73e06cd57b449525845466624eb9a
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-22 16:42:15 +0100

    Update host-1-a.sh

--- host-1-a.sh
+++ host-1-a.sh
@@ -1,2 +1,7 @@
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump --assume-yes
+apt install -y curl --assume-yes
+ip addr add 7.7.10.1/30 dev eth1
+ip link set eth1 up
+
