f86306e75958aa236ef250f8fc04fe929930d04a
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-25 17:09:18 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -8,13 +8,14 @@ apt-get install -y frr --assume-yes --force-yes
 
 ip link add link eth1 name eth1.10 type vlan id 10
 ip link add link eth2 name eth1.20 type vlan id 20
-ip addr add 7.7.10.62/26 dev eth1.10 
-ip addr add 7.7.20.254/23 dev eth1.20
 ip link set dev eth1.10 up
 ip link set dev eth1.20 up
+ip addr add 7.7.10.62/26 dev eth1.10 
+ip addr add 7.7.20.254/23 dev eth1.20
 
-ip addr add 7.7.30.1/30 dev eth2 
 ip link set dev eth2 up
+ip addr add 7.7.30.1/30 dev eth2 
+
 
 
 
