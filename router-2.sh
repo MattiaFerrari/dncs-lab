1cb54c5026d548acf7974c5aebd07c106b76f822
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-26 10:48:22 +0100

    Update router-2.sh

--- router-2.sh
+++ router-2.sh
@@ -1,25 +1,11 @@
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y tcpdump apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
-wget -O- https://apps3.cumulusnetworks.com/setup/cumulus-apps-deb.pubkey | apt-key add -
-add-apt-repository "deb [arch=amd64] https://apps3.cumulusnetworks.com/repos/deb $(lsb_release -cs) roh-3"
-apt-get update
-apt-get install -y frr --assume-yes --force-yes
 
 ip link set dev eth1 up
-ip link set dev eth2 up
 ip addr add 7.7.40.254/23 dev eth1 
+
+ip link set dev eth2 up
 ip addr add 7.7.30.2/30 dev eth2 
 
-
-
-
-sysctl net.ipv4.ip_forward=1
-sed -i 's/zebra=no/zebra=yes/g' /etc/frr/daemons
-sed -i 's/ospfd=no/ospfd=yes/g' /etc/frr/daemons
-service frr restart
-vtysh -c 'configure terminal' -c 'interface eth2' -c 'ip ospf area 0.0.0.0'
-vtysh -c 'configure terminal' -c 'router ospf' -c 'redistribute connected'
-
-ip route add 7.7.10.0 via 7.7.30.1/30   dev eth2
-ip route add 7.7.20.0 via 7.7.30.1/30   dev eth2 
+sysctl -w net.ipv4.ip_forward=1 > /dev/null
+ip route add 7.7.10.0/26 via 7.7.30.1/30   dev eth2
+ip route add 7.7.20.0/23 via 7.7.30.1/30   dev eth2 
