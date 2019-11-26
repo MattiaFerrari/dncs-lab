7689c22faff17c647e23913f5098a6fa65c75c67
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-26 11:34:47 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -1,11 +1,11 @@
 export DEBIAN_FRONTEND=noninteractive
 
-ip link set dev eth1 up
-ip addr add 7.7.10.62/26 dev eth1
-ip addr add 7.7.20.254/23 dev eth1
+ip link set dev enp0s8 up
+ip addr add 7.7.10.62/26 dev enp0s8
+ip addr add 7.7.20.254/23 dev enp0s8
 
-ip link set dev eth2 up
-ip addr add 7.7.30.1/30 dev eth2 
+ip link set dev enp0s9 up
+ip addr add 7.7.30.1/30 dev enp0s9 
 
 sysctl -w net.ipv4.ip_forward=1 > /dev/null
-ip route add 7.7.40.0/23 via 7.7.30.2/30 dev eth2 
+ip route add 7.7.40.0/23 via 7.7.30.2/30 dev enp0s9 
