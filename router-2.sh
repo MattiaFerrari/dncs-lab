5bda20a2bc27711c1c0231f38522ec42e5ef42ef
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-26 11:44:03 +0100

    Update router-2.sh

--- router-2.sh
+++ router-2.sh
@@ -7,5 +7,5 @@ ip link set dev enp0s9 up
 ip addr add 7.7.30.2/30 dev enp0s9 
 
 sysctl -w net.ipv4.ip_forward=1 > /dev/null
-ip route add 7.7.10.0/26 via 7.7.30.1/30   dev enp0s9
-ip route add 7.7.20.0/23 via 7.7.30.1/30   dev enp0s9 
+ip route add 7.7.10.0/26 via 7.7.30.1   dev enp0s9
+ip route add 7.7.20.0/23 via 7.7.30.1   dev enp0s9 
