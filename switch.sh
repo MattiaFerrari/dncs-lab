ceb2ab636b94bc57a4bf3d7a89c7767fd4b5f454
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-26 10:50:52 +0100

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -2,11 +2,15 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump
 apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-certificates curl software-properties-common
+
 ovs-vsctl add-br switch
+
 ovs-vsctl add-port switch eth1
-ovs-vsctl add-port switch eth2 tag=10
-ovs-vsctl add-port switch eth3 tag=20
+ovs-vsctl add-port switch eth2
+ovs-vsctl add-port switch eth3
+
 ip link set eth1 up
 ip link set eth2 up
 ip link set eth3 up
+
 ip link set dev ovs-system up
