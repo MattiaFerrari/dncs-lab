c98550550f22c9afaaf2f506cca914edca4e53ac
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-26 12:18:41 +0100

    Update host-2-c.sh

--- host-2-c.sh
+++ host-2-c.sh
@@ -6,7 +6,7 @@ apt-get -qq update
 apt-get -qq install docker-ce docker-ce-cli containerd.io
 
 ip link set dev enp0s8 up
-ip addr add 7.7.20.1/23 dev enp0s8
+ip addr add 7.7.40.1/23 dev enp0s8
 
 ip route add 7.7.20.0/23 via 7.7.40.254
 ip route add 7.7.10.0/26 via 7.7.40.254
