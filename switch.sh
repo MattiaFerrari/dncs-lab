56c8b6620e8a8678b964281b7f9489c6e53375d2
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-26 11:18:43 +0100

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -5,12 +5,12 @@ apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-
 
 ovs-vsctl add-br switch
 
-ovs-vsctl add-port switch en1
-ovs-vsctl add-port switch en2
-ovs-vsctl add-port switch en3
+ovs-vsctl add-port switch eth1
+ovs-vsctl add-port switch eth2
+ovs-vsctl add-port switch eth3
 
-ip link set en1 up
-ip link set en2 up
-ip link set en3 up
+ip link set eth1 up
+ip link set eth2 up
+ip link set eth3 up
 
 ip link set dev ovs-system up
