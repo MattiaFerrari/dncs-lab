a14f625e24655b97def30d3e5d702400905cdfe4
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-26 12:27:41 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -1,8 +1,12 @@
 export DEBIAN_FRONTEND=noninteractive
 
+ip link add link enp0s8 name enp0s8.1 type vlan id 1
+ip link add link enp0s8 name enp0s88.2 type vlan id 2
 ip link set dev enp0s8 up
-ip addr add 7.7.10.62/26 dev enp0s8
-ip addr add 7.7.20.254/23 dev enp0s8
+ip link set dev enp0s8.1 up
+ip link set dev enp0s8.2 up
+ip addr add 7.7.10.62/26 dev enp0s8.1
+ip addr add 7.7.20.254/23 dev enp0s8.2
 
 ip link set dev enp0s9 up
 ip addr add 7.7.30.1/30 dev enp0s9 
