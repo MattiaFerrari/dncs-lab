733371c9dd012affe451cf673a3b7c21ec33eb3b
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-27 10:39:29 +0100

    assignment done

--- router-2.sh
+++ router-2.sh
@@ -7,5 +7,6 @@ ip link set dev enp0s9 up
 ip addr add 7.7.30.2/30 dev enp0s9 
 
 sysctl -w net.ipv4.ip_forward=1 > /dev/null
+
 ip route add 7.7.10.0/26 via 7.7.30.1
 ip route add 7.7.20.0/23 via 7.7.30.1  
