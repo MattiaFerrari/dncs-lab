cbb7f337af68046d29840fb2bd323eb3b27c8c3a
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-27 10:59:39 +0100

    Update host-2-c.sh

--- host-2-c.sh
+++ host-2-c.sh
@@ -1,15 +1,17 @@
 export DEBIAN_FRONTEND=noninteractive
-#sudo apt-get update
-#sudo apt-get install -y tcpdump --assume-yes
-#sudo apt install -y curl --assume-yes
-#sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
-#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-#sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#sudo apt-get update
-#sudo apt-get install -y docker-ce --assume-yes --force-yes
+apt-get update -y
+apt-get install -y tcpdump --assume-yes
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce --assume-yes --force-yes
 
-#docker run -d -p 80:80 dustnic82/nginx-test
+docker kill $(docker ps -q)
+docker rm $(docker ps -aq)
+docker pull -q dustnic82/nginx-test
+docker run -d -p 80:80 dustnic82/nginx-test
+
 ip link set dev enp0s8 up
 ip addr add 7.7.40.1/23 dev enp0s8
 
