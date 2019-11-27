bf1742d6846cf17017f6d5c31e691b424aea78d4
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-27 10:38:21 +0100

    assignment done

--- router-1.sh
+++ router-1.sh
@@ -2,6 +2,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 ip link add link enp0s8 name enp0s8.10 type vlan id 10
 ip link add link enp0s8 name enp0s8.20 type vlan id 20
+
 ip link set dev enp0s8 up
 ip link set dev enp0s8.10 up
 ip link set dev enp0s8.20 up
