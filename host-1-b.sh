3c119210a3ea0e175b222922a3bf3a1282da207f
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-28 11:32:05 +0100

    Update host-1-b.sh

--- host-1-b.sh
+++ host-1-b.sh
@@ -3,6 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 ip link set dev enp0s8 up
 ip addr add 7.7.20.1/23 dev enp0s8
 
-ip route add default via 7.7.20.254
+ip route replace 7.7.40.1/23 via 7.7.20.254
 
 
