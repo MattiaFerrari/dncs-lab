d4131d9f010cab843ec3dbffdafc9c9f84b62138
MattiaFerrari <58030265+MattiaFerrari@users.noreply.github.com>
2019-11-28 11:33:10 +0100

    Update host-2-c.sh

--- host-2-c.sh
+++ host-2-c.sh
@@ -15,7 +15,8 @@ docker run -d -p 80:80 dustnic82/nginx-test
 ip link set dev enp0s8 up
 ip addr add 7.7.40.1/23 dev enp0s8
 
-ip route add default via 7.7.40.254
+ip route replace 7.7.10.1/26 via 7.7.40.254
+ip route replace 7.7.20.1/23 via 7.7.40.254
 
 
 
